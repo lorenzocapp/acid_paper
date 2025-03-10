ecf488406e26405e3c98d18b635cbf22bf4b8b7d
Lorenzo Cappello <38407081+lorenzocapp@users.noreply.github.com>
2025-03-10 18:04:40 +0100

    before other changes

--- functions_kernel.R
+++ functions_kernel.R
@@ -53,9 +53,24 @@ ps_kernel <- function(y,M,h,kernel="gaussian"){
   
   n <- length(y)
   Y <- y
-  for (i in 1:M){
-    j <- sample(n+i-1,1) #determine from which kernel to sample
-    Y <- c(Y,rnorm(1,mean=Y[j],sd=h[j]))
+  if (kernel=="gaussian"){
+    for (i in 1:M){
+      j <- sample(n+i-1,1) 
+      Y <- c(Y,rnorm(1,mean=Y[j],sd=h[j]))
+    }
+  } else if (kernel=="uniform"){
+    for (i in 1:M){
+      j <- sample(n+i-1,1) 
+      Y <- c(Y,runif(1,min=Y[j]-h[j],max=Y[j]+h[j]))
+    }
+  } else if (kernel=="laplace"){
+    #h <- 1.1 *h #inflate kernel for Laplace
+    for (i in 1:M){
+      j <- sample(n+i-1,1) 
+      Y <- c(Y,rlaplace(1,location=Y[j],scale=h[j]))
+    }
+  } else {
+    print("Kernel not yet implemented!")
   }
     boot <- Y[n+1:M]
   
@@ -73,16 +88,193 @@ recursive_kernel <- function(y,grid,h,kernel="gaussian",ps=TRUE,M=1000){
     y  <- c(y,yB)
   }
   
+  
   if (kernel=="gaussian"){
-  f <- dnorm((grid-y[1])/h[1])/h[1]
-  for (i in 2:length(y)){
-    f <- (1-i^(-1))*f + i^(-1)/h[i]* dnorm((grid-y[i])/h[i])
-    Z <- (grid[2]-grid[1])/2*(2*sum(f)-f[1]-f[length(f)])
-    f <- f/Z
-  }}
+    f <- dnorm((grid-y[1])/h[1])/h[1]
+    for (i in 2:length(y)){
+      f <- (1-i^(-1))*f + i^(-1)/h[i]* dnorm((grid-y[i])/h[i])
+      #Z <- (grid[2]-grid[1])/2*(2*sum(f)-f[1]-f[length(f)])
+      #f <- f/Z
+    }
+  } else if (kernel=="uniform"){
+    f <- dunif(grid,min=y[1]-h[1],max=y[1]+h[1])
+    for (i in 2:length(y)){
+      f <- (1-i^(-1))*f + i^(-1)*dunif(grid,min=y[i]-h[i],max=y[i]+h[i])
+      #Z <- (grid[2]-grid[1])/2*(2*sum(f)-f[1]-f[length(f)])
+      #f <- f/Z
+    }
+  } else if (kernel=="laplace"){
+    #h <- 1.1*h #inflate the kernel for Laplace
+    f <- dlaplace(grid,location=y[1],scale=h[1])
+    for (i in 2:length(y)){
+      f <- (1-i^(-1))*f + i^(-1)*dlaplace(grid,location=y[i],scale=h[i])
+    }
+    #we adjust to make it sum to one at the end
+    #Z <- (grid[2]-grid[1])/2*(2*sum(f)-f[1]-f[length(f)])
+    #f <- f/Z
+  } else {
+    print("Kernel not yet implemented!")
+  }  
   return(f)
 }
 
+
+
+standard_kernel <- function(y,grid,h,kernel="gaussian",bw="nrd",ps=TRUE,M=1000){
+  
+  'Implement the kernel standard estimator (unless it is Gaussian, since it already exists): it can generate predictive resampling samples!'
+  
+  #1.Predictive resampling
+  n <- length(y)
+  if (ps==TRUE){
+    yB <- ps_kernel(y,M,h,kernel)
+    y  <- c(y,yB)
+  }
+  #2. Determines optimal bw at the end
+  bw_function <- match.fun(paste0("bw.", bw))
+  h0 <- bw_function(y)
+  
+  #3. Do kde with optimal bw and data+synthetic data
+  if (kernel=="gaussian"){
+    f <- gaussian_kde(y,grid,h0=h0)
+  } else if (kernel=="uniform"){
+    f <- uniform_kde(y,grid,h0=h0)
+  } else if (kernel=="laplace"){
+    f <- laplace_kde(y,grid,h0=h0)
+  } else {
+    print("Kernel not yet implemented!")
+  }  
+  return(f)
+}
+
+
+
+laplace_kde <- function(y,grid,h0) {
+  
+  "Implements a standard Kde"
+  
+  # laplace kernel function
+  f <- dlaplace(grid,location=y[1],scale=h0)
+  for (i in 2:length(y)){
+    f <- (1-i^(-1))*f + i^(-1)*dlaplace(grid,location=y[i],scale=h0)
+  }
+  #adjust to make it sum to one at the end
+  Z <- (grid[2]-grid[1])/2*(2*sum(f)-f[1]-f[length(f)])
+  f <- f/Z
+
+  return(f)
+}
+
+uniform_kde <- function(y,grid,h0) {
+  
+  "Implements a standard Kde"
+  
+  # uniform kernel function
+  f <- dunif(grid,min=y[1]-h0,max=y[1]+h0)
+  for (i in 2:length(y)){
+    f <- (1-i^(-1))*f + i^(-1)*dunif(grid,min=y[i]-h0,max=y[i]+h0)
+    Z <- (grid[2]-grid[1])/2*(2*sum(f)-f[1]-f[length(f)])
+    f <- f/Z
+  }
+  return(f)
+}
+
+
+gaussian_kde <- function(y,grid,h0) {
+  
+  "Implements a standard Kde"
+  
+  # Gaussian kernel function
+  f <- dnorm((grid-y[1])/h0)/h0
+  for (i in 2:length(y)){
+    f <- (1-i^(-1))*f + i^(-1)/h0* dnorm((grid-y[i])/h0)
+    Z <- (grid[2]-grid[1])/2*(2*sum(f)-f[1]-f[length(f)])
+    f <- f/Z
+  }
+  return(f)
+}
+
+ps_posterior <- function(y,grid,h,M=1000,B=100,kernel="gaussian",algo="recursive",bw="ndr"){
+  
+  "Implement Predictive resampling posterior
+  Inputs:
+  - algo: recursive it should use the recursive kernel estimator, standard if uses the standard kernel density estimatr
+  Returns:
+  - mean
+  - median
+  - 97.5% and 2.5% bands"
+  
+  posterior <- matrix(0,ncol=length(grid),nrow=B)
+  if (algo=="recursive") {
+    for (b in 1:B){
+      posterior[b,] <- recursive_kernel(y,grid,h,kernel,ps=TRUE,M)
+    }
+  } else if (algo=="standard"){
+    for (b in 1:B){
+      posterior[b,] <- standard_kernel(y,grid,h,kernel,M,bw,ps=TRUE,)
+    }
+  }
+  
+  
+  q2.5 <- apply(data.frame(posterior), 2, quantile, probs=0.025)
+  q97.5 <- apply(data.frame(posterior), 2, quantile, probs=0.975)
+  q50 <- apply(data.frame(posterior), 2, quantile, probs=0.5)
+  mean <- apply(data.frame(posterior), 2, mean)
+  posterior <- list(grid=grid,mean=mean,q2.5=q2.5,q97.5=q97.5,q50=q50)
+  return(posterior)
+}
+
+
+
+
+
+
+plot_ps_posterior <- function(grid,posterior,true,ylim=c(),xlim=c()){
+
+  if (length(ylim)==0){
+    ylim = c(0,max(c(posterior$q97.5,true)))
+  }
+  if (length(xlim)==0){
+    xlim <- c(min(grid),max(grid))
+  }
+  plot(grid,posterior$mean,type="l",ylim=ylim,xlim=xlim,lwd=2)
+  polygon(c(grid, rev(grid)), c(posterior$q2.5, rev(posterior$q97.5)),
+          col = "#D3D3D3")
+  lines(grid,posterior$mean,lwd=2)
+  lines(grid,true, lty=2,lwd=2)
+  lines(grid,posterior$q2.5,col="black")
+  lines(grid,posterior$q97.5,col="black")
+}
+
+
+plot_ps_fong <- function(mat,grid,grid_fong,true,ylim=c(),xlim=c()){
+  
+  q2.5 <- apply(data.frame(mat), 2, quantile, probs=0.025)
+  q97.5 <- apply(data.frame(mat), 2, quantile, probs=0.975)
+  q50 <- apply(data.frame(mat), 2, quantile, probs=0.5)
+  mean <- apply(data.frame(mat), 2, mean)
+  posterior <- list(grid_fong=grid_fong,mean=mean,q2.5=q2.5,q97.5=q97.5,q50=q50)
+  if (length(ylim)==0){
+    ylim = c(0,max(c(posterior$q97.5,true)))
+  }
+  if (length(xlim)==0){
+    xlim <- c(min(grid_fong),max(grid_fong))
+  }
+  plot(grid_fong,posterior$mean,type="l",ylim=ylim,xlim=xlim,lwd=2)
+  polygon(c(grid_fong, rev(grid_fong)), c(posterior$q2.5, rev(posterior$q97.5)),
+          col = "#D3D3D3")
+  lines(grid_fong,posterior$mean,lwd=2)
+  lines(grid,true, lty=2,lwd=2)
+  lines(grid_fong,posterior$q2.5,col="black")
+  lines(grid_fong,posterior$q97.5,col="black")
+}
+
+
+
+
+##################################
+######## UNUSED (FOR NOW) ########
+##################################
 
 #Not sure if necessary for now
 recursive_kernel_one <- function(f,grid,y,h,i,kernel="gaussian"){
@@ -99,36 +291,3 @@ recursive_kernel_one <- function(f,grid,y,h,i,kernel="gaussian"){
   return(f)
 }
 
-ps_posterior <- function(y,grid,h,M=1000,B=100,kernel="gaussian"){
-  
-  "Implement Predictive resampling posterior
-  Returns:
-  - mean
-  - median
-  - 97.5% and 2.5% bands"
-  
-  posterior <- matrix(0,ncol=length(grid),nrow=B)
-  for (b in 1:B){
-    posterior[b,] <- recursive_kernel(y,grid,h,kernel="gaussian",ps=TRUE,M)
-  }
-  
-  q2.5 <- apply(data.frame(posterior), 2, quantile, probs=0.025)
-  q97.5 <- apply(data.frame(posterior), 2, quantile, probs=0.975)
-  q50 <- apply(data.frame(posterior), 2, quantile, probs=0.5)
-  mean <- apply(data.frame(posterior), 2, mean)
-  posterior <- list(grid=grid,mean=mean,q2.5=q2.5,q97.5=q97.5,q50=q50)
-  return(posterior)
-}
-
-
-plot_ps_posterior <- function(grid,posterior,true){
-
-  ymax <- max(c(posterior$q97.5,true))
-  plot(grid,posterior$mean,type="l",ylim=c(0,ymax),lwd=2)
-  polygon(c(grid, rev(grid)), c(posterior$q2.5, rev(posterior$q97.5)),
-          col = "#D3D3D3")
-  lines(grid,posterior$mean,lwd=2)
-  lines(grid,true, lty=2,lwd=2)
-  lines(grid,posterior$q2.5,col="black")
-  lines(grid,posterior$q97.5,col="black")
-}
--- master_kernel_dev.R
+++ master_kernel_dev.R
@@ -1,32 +1,42 @@
 rm(list=ls())
 source("functions_kernel.R")
+
+library(diagL1)
+
+idx <- 1
 #sample data
-n <- 200
+n <- 50
 
 u <- runif(n)
 y <- rnorm(n,-2,1)
 y1 <- rnorm(n,2,1)
 y[u>0.8] <- y1[u>0.8]
 #y <- (y-mean(y))/sd(y)
+name <- paste("data/data_",idx,".csv",sep="")
+write.csv(y,"data/data.csv",row.names = FALSE)
+M <- 500
+B <- 400
+characteristics <- data.frame(M=M,B=B)
 
 
 #grid
-xmin=-8
-xmax=8
-ngrid=200
-grid <- seq(xmin,xmax,(xmax-xmin)/ngrid)
+#xmin=-8
+#xmax=8
+#ngrid=200
+grid <- seq(-10,10.95,0.05)*sd(y)+mean(y) #it is an automatic grid, so the Gaussian copula is identical
 true <- 0.8*dnorm(grid,-2,1)+0.2*dnorm(grid,2,1)
 
 
 
 #Run
 M <- 500
-h <- initialization_kernel_resampling(y,M,bw="SJ")
-posterior <- ps_posterior(y,grid,h,M,B=400,kernel="gaussian")
-plot_ps_posterior(grid,posterior,true)
+h <- initialization_kernel_resampling(y,M,bw="nrd")
+posterior <- ps_posterior(y,grid,h,M,B,kernel="laplace")
+plot_ps_posterior(grid,posterior,true,xlim=c(-6,6))
 
-
-
+#h0=h[1]
+#hist(y,breaks=50,freq=FALSE)
+#lines(grid,laplace_kde(y,grid,h0))
 
 
 #R functions to select bandwidth with Gaussian kernel
@@ -45,3 +55,28 @@ plot(density(y,kernel="gaussian",bw=bw.ucv(y)))
 lines(grid,true)
 
 
+
+###Python practice
+library(reticulate)
+#py_require("pr_copula")
+#py_require("numpy")
+#py_install("pr_copula")
+#py_require("jax")#this runs and check which packages are necessary
+#source_python("python_codes/1_univariate_copula_modified.py")
+#source_python("python_codes/bivariate_copula.py")
+#source_python("python_codes/copula_density_functions.py")
+#config <- reticulate::py_config()
+#system2(config$python, c("-m", "pip", "install", "--quiet", shQuote("./")))
+
+source_python("master_python.py")
+
+y <- as.matrix(y)
+check <- run_copula_algo(y,100,100,n)
+
+np <- import("numpy")
+mat <- np$load("output/trial.npy")
+mat <- np$matrix(exp(mat))/sd(y)
+
+
+grid_fong <- seq(-4,3.95,0.05)*sd(y)+mean(y)#taken from experiments_plots notebook
+plot_ps_fong(mat,grid,grid_fong,true,ylim=c(0,0.4),xlim=c(-6,6))
